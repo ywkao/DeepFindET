3bdba3095f714cb7c19ea574832013ee3816cd0b
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2025-01-26 08:50:36 +0100

    add missing numpy()

--- deepfindET/training_copick.py
+++ deepfindET/training_copick.py
@@ -421,7 +421,7 @@ class Train(core.DeepFindET):
             LearningRateParameters: A Pydantic model containing the learning rate parameters.
         """        
         return settings.LearningRateParameters(
-            learning_rate=float(self.net.optimizer.inner_optimizer._learning_rate),
+            learning_rate=float(self.net.optimizer.inner_optimizer._learning_rate.numpy()),
             min_learning_rate=callback.min_lr,
             monitor=callback.monitor,
             factor=callback.factor,
